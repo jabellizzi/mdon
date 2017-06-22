3029a34c55ed7084059069a14d4598c07228cae1
John Bellizzi <jabellizzi406@gmail.com>
2017-06-22 14:19:59 -0400

    parsing markdown

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import MarkdownIt from 'markdown-it';
+import chapter1 from './markdown-input/chapter101.js';
 
+var md = new MarkdownIt();
+
+var render = md.render(chapter1);
+var parse = md.parse(chapter1);
+
+console.log(render);
+console.log(parse);
+
+var object = {
+  chapter: 1,
+  content: []
+};
+
+parse.forEach((parsedItem, i) =>{
+  if(parsedItem.nesting === 1){
+    object.content.push({
+      tag: parsedItem.tag,
+      level: parsedItem.level
+    })
+  }
+})
+
+console.log(object);
+// console.log(JSON.stringify(object));
