066bc3f987acee2566db5d464cafe1af41fb56de
John Bellizzi <jabellizzi406@gmail.com>
2017-07-17 17:16:46 -0400

    remove image id name from image class

--- src/lib/dataPrep.js
+++ src/lib/dataPrep.js
@@ -67,7 +67,7 @@ export default function dataPrep(inputArray){
           var source = child.attrs[0][1].split('/');
           var id = source[source.length - 1].split('.')[0];
 
-          child.attrs.push(['class', 'img-responsive ' +id]);
+          child.attrs.push(['class', 'img-responsive']);
           child.children = null;
           child.nesting = 1;
           child.level = 1;
